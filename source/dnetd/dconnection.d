8890cd3e79c7c44db5bcbfd2ee91b01eb0a373f0
Tristan 🅱. Kildaire <21629986@sun.ac.za>
2020-09-23 10:33:10 +0200

    Updated dependancies

--- source/dnetd/dconnection.d
+++ source/dnetd/dconnection.d
@@ -12,11 +12,20 @@ module dnetd.dconnection;
 
 import core.thread : Thread;
 import std.socket : Socket;
+import bmessage;
+import tristanable.encoding : DataMessage;
+
 
 public class DConnection : Thread
 {
-	/* The client's socket */
+	/**
+	* Connection information
+	*/
 	private Socket socket;
+	private bool hasAuthed;
+
+	/* Reserved tag for push notifications */
+	private long notificationTag = 0;
 
 	this(Socket socket)
 	{
@@ -26,8 +35,20 @@ public class DConnection : Thread
 		/* Set the socket */
 		this.socket = socket;
 
+		/* Initialize the tagging facility */
+		initTagger();
+
 		/* Start the connection handler */
 		start();
+	}
+
+	/**
+	* Initializes tristanable
+	* TODO: Implemet me (also tristanable needs reserved tags first)
+	*/
+	private void initTagger()
+	{
+		
 	}
 
 	/**
@@ -35,9 +56,56 @@ public class DConnection : Thread
 	*/
 	private void worker()
 	{
+		/* Received bytes (for bformat) */
+		byte[] receivedBytes;
+
+		/* Received message */
+		DataMessage receivedMessage;
+
 		while(true)
+		{		
+			/**
+			* Block to receive a bformat message
+			*
+			* (Does decoding for bformat too)
+			*/
+			bool status = receiveMessage(socket, receivedBytes);
+
+			/* TODO: Check status */
+
+			/* Decode the tristanable message (tagged message) */
+			receivedMessage = DataMessage.decode(receivedBytes);
+
+			/* TODO: Tristanable needs reserved-tag support (client-side concern) */
+		}
+	}
+
+	/**
+	* Process the received message
+	*/
+	private void process(DataMessage message)
+	{
+		/* Get the command byte */
+		byte commandByte = message.data[0];
+
+		/* If `auth` command */
+		if(commandByte == 0 && !hasAuthed)
+		{
+			
+		}
+		/* If `link` command */
+		else if(commandByte == 1 && !hasAuthed)
+		{
+			
+		}
+		/* TODO: Handle this case */
+		else
 		{
 			
 		}
 	}
+
+	/**
+	*
+	*/
 }
