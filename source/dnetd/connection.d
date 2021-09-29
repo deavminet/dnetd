499a0f8935f0e6466686556f37b6d5fa8bed4270
Tristan B. Kildaire <deavmi@disroot.org>
2021-09-29 14:26:54 +0200

    Added some notes on connection handling for a job-mechanism

--- source/dnetd/connection.d
+++ source/dnetd/connection.d
@@ -18,10 +18,36 @@ public class Connection : Thread
     private Socket socket;
 
     /**
-    * 
+    * Creates a new handler for a client Connection
     */
     this(Socket clientSocket)
     {
+        super(&run);
         socket = clientSocket;
+
+        /* Add myself to the server's connection queue */
+        server.addConnection(this);
+
+        start();
+    }
+
+    private void run()
+    {
+        while(true)
+        {
+            /* TODO: Add handling here */
+            /**
+            * TODO
+            *
+            * Here we want to bformat receive, then get a tristanable tag
+            * (for later replying), then get the remaining message (the
+            * command) and pass that to `process(message, tag)`, it can
+            * then reply by tristanable-tag encoding and bformat encoding
+            * and send over the socket
+            *
+            * The `process` call must spawn a new thread to handle all of
+            * that
+            */
+        }
     }
 }
